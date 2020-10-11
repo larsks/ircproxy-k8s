53af90c0782fade2006324ecb34b46876d23e04e
Lars Kellogg-Stedman <lars@oddbit.com>
2020-10-10 20:47:17 -0400

    match uid used in base securityContext

--- oddbit/init-bitlbee.sh
+++ oddbit/init-bitlbee.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+set -e
+
 if [ ! -d /var/lib/bitlbee/.git ]; then
     echo "installing bitlbee configuration"
     GIT_SSH_COMMAND='ssh -o stricthostkeychecking=no -i /tmp/deploykeys/bitlbee-config-deploy' \
     git -C /var/lib/bitlbee clone git@github.com:larsks/ircproxy-bitlbee-config.git .
+fi
+
+if [ "$(id -u)" -eq 0 ]; then
+	chown -R 1000 /var/lib/bitlbee
 fi
--- oddbit/init-znc.sh
+++ oddbit/init-znc.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 if [ ! -d /znc-data/.git ]; then
     echo "installing znc configuration"
     GIT_SSH_COMMAND='ssh -o stricthostkeychecking=no -i /tmp/deploykeys/znc-config-deploy' \
@@ -7,4 +9,8 @@ if [ ! -d /znc-data/.git ]; then
 fi
 
 echo "installing znc certificate"
-cp /data/znc-certificate/znc.pem /znc/znc.pem
+cp /data/znc-certificate/znc.pem /znc-data/znc.pem
+
+if [ "$(id -u)" -eq 0 ]; then
+	chown -R 1000 /znc-data
+fi
