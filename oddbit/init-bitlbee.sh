2c76389fba6922f4bbb4cd1cc638f48b78e0d6aa
Lars Kellogg-Stedman <lars@oddbit.com>
2020-10-12 21:41:50 -0400

    clone to temporary directory and copy
    git is sensitive about non-emtpy directories (which we get if say we
    mount a log volume inside the config volume)

--- oddbit/init-bitlbee.sh
+++ oddbit/init-bitlbee.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-if [ ! -d /var/lib/bitlbee/.git ]; then
+DATADIR=/var/lib/bitlbee
+
+if [ ! -d $DATADIR/larsks.xml ]; then
     echo "installing bitlbee configuration"
     GIT_SSH_COMMAND='ssh -o stricthostkeychecking=no -i /tmp/deploykeys/bitlbee-config-deploy' \
-    git -C /var/lib/bitlbee clone git@github.com:larsks/ircproxy-bitlbee-config.git .
+    git clone git@github.com:larsks/ircproxy-bitlbee-config.git /tmp/init-data
+    tar -C /tmp/init-data -cf- . | tar -C $DATADIR -xf-
 fi
 
 if [ "$(id -u)" -eq 0 ]; then
-	chown -R 1000 /var/lib/bitlbee
+	chown -R 1000 $DATADIR
 fi
--- oddbit/init-znc.sh
+++ oddbit/init-znc.sh
@@ -2,15 +2,18 @@
 
 set -e
 
-if [ ! -d /znc-data/.git ]; then
+DATADIR=/znc-data
+
+if [ ! -d $DATADIR/configs/znc.conf ]; then
     echo "installing znc configuration"
     GIT_SSH_COMMAND='ssh -o stricthostkeychecking=no -i /tmp/deploykeys/znc-config-deploy' \
-    git -C /znc-data clone git@github.com:larsks/ircproxy-znc-config.git .
+    git clone git@github.com:larsks/ircproxy-znc-config.git /tmp/init-data
+    tar -C /tmp/init-data -cf- . | tar -C $DATADIR -xf-
 fi
 
 echo "installing znc certificate"
-cp /data/znc-certificate/znc.pem /znc-data/znc.pem
+cp /data/znc-certificate/znc.pem $DATADIR/znc.pem
 
 if [ "$(id -u)" -eq 0 ]; then
-	chown -R 1000 /znc-data
+	chown -R 1000 $DATADIR
 fi
